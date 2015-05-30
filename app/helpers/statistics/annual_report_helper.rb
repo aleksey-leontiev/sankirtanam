4bfd106e722975f8393bd4f8358e2458fc178d9a
Aleksey Leontiev <aleksey.leontiev@icloud.com>
2015-05-30 11:25:48 +0000

    fix for win

--- app/helpers/statistics/annual_report_helper.rb
+++
@@ -1,9 +0,0 @@
-module Statistics::AnnualReportHelper
-  # converts overall data for chart
-  def annual_chart_data(data)
-    range  = [1,2,3,4,5,6,7,8,9,10,11,12]
-    [range.map{ |x|
-      (data.detect{ |y| y[:date].month == x } or { quantity:0 })[:quantity]
-    }].to_json
-  end
-end
--- app/helpers/statistics/location_report_helper.rb
+++
@@ -1,9 +0,0 @@
-module Statistics::LocationReportHelper
-  # converts overall data for chart
-  def location_chart_data(data)
-    range  = [1,2,3,4,5,6,7,8,9,10,11,12]
-    [range.map{ |x|
-      (data.detect{ |y| y[:date].month == x } or { quantity:0 })[:quantity]
-    }].to_json
-  end
-end
